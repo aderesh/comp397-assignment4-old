591e210c99957e6a9a5ab67ca37ae5fe26b31f52
andrii deresh <deresh.andrew@hotmail.com>
2016-07-11 09:23:21 -0400

    added referecnes added example of createjs code

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,5 +1,24 @@
+/// <reference path="_reference.ts"/>
+var canvas = document.getElementById("canvas");
+var stage;
+var helloLabel;
 function init() {
-    console.log("aaa");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", gameloop);
+    main();
+}
+function gameloop() {
+    helloLabel.rotation += 5;
+    stage.update();
+}
+function main() {
+    helloLabel = new createjs.Text("goodbye", "40px Consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
 }
 window.addEventListener("load", init);
 //# sourceMappingURL=game.js.map
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,5 +1,35 @@
+/// <reference path="_reference.ts"/>
+
+let canvas:HTMLElement = document.getElementById("canvas");
+let stage:createjs.Stage;
+
+let helloLabel:createjs.Text;
+
 function init():void {
-    console.log("aaa");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", gameloop);
+
+    main();
+}   
+
+function gameloop():void {
+
+    helloLabel.rotation += 5;
+
+    stage.update();
+}
+
+function main():void {
+    helloLabel = new createjs.Text("goodbye","40px Consolas", "#000000")
+
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+
+    stage.addChild(helloLabel);
 }
 
 window.addEventListener("load", init);
